ff90c9f05e5c7e8f924ea8bdacff473e7ca1bf5f
TheBestPessimist <cristian@tbp.land>
2019-05-23 21:48:26 +0300

    Add solution for https://codeforces.com/contest/1157/problem/B

---
+++ src/test/kt/contest/_1157/B_Long_Number_Test.kt
@@ -0,0 +1,50 @@
+package kt.contest._1157
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+@Suppress("ClassName", "UnnecessaryVariable")
+internal class B_Long_Number_Test {
+
+    @Test
+    fun example1() {
+        val s1 = "4"
+        val s2 = "1337"
+        val s3 = "1 2 5 4 6 6 3 1 9"
+        val expected = "1557"
+
+        val n = s1
+        val a = makeA(s2)
+        val f = makeF(s3)
+
+        assertEquals(expected, calculateMax(n, a, f))
+    }
+
+    @Test
+    fun example2() {
+        val s1 = "5"
+        val s2 = "11111"
+        val s3 = "9 8 7 6 5 4 3 2 1"
+        val expected = "99999"
+
+        val n = s1
+        val a = makeA(s2)
+        val f = makeF(s3)
+
+        assertEquals(expected, calculateMax(n, a, f))
+    }
+
+    @Test
+    fun example3() {
+        val s1 = "2"
+        val s2 = "33"
+        val s3 = "1 1 1 1 1 1 1 1 1"
+        val expected = "33"
+
+        val n = s1
+        val a = makeA(s2)
+        val f = makeF(s3)
+
+        assertEquals(expected, calculateMax(n, a, f))
+    }
+}
