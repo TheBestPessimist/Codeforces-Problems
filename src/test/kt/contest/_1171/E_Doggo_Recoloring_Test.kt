f1d9cc4aaa60824ca5baeb42ee3f972e4f3e8917
TheBestPessimist <cristian@tbp.land>
2019-05-25 16:47:09 +0300

    Add solution for https://codeforces.com/contest/1171/problem/E

---
+++ src/test/kt/contest/_1171/E_Doggo_Recoloring_Test.kt
@@ -0,0 +1,61 @@
+package kt.contest._1171
+
+import kt.toListOfString
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class E_Doggo_Recoloring_Test {
+
+    @Test
+    fun tryColorThem1() {
+        val doggos = "aabddc".toListOfString()
+        val exp = true
+        assertEquals(exp, tryColorThem(doggos))
+    }
+
+
+    @Test
+    fun tryColorThem2() {
+        val doggos = "abc".toListOfString()
+        val exp = false
+        assertEquals(exp, tryColorThem(doggos))
+    }
+
+    @Test
+    fun tryColorThem3() {
+        val doggos = "jjj".toListOfString()
+        val exp = true
+        assertEquals(exp, tryColorThem(doggos))
+    }
+
+    @Test
+    fun tryColorThem4() {
+        val doggos = "".toListOfString()
+        val exp = true
+        assertEquals(exp, tryColorThem(doggos))
+    }
+
+    @Test
+    fun tryColorThem5() {
+        val doggos = "a".toListOfString()
+        val exp = true
+        assertEquals(exp, tryColorThem(doggos))
+    }
+
+    @Test
+    fun tryColorThem6() {
+        val doggos = "abababc".toListOfString()
+        val exp = true
+        assertEquals(exp, tryColorThem(doggos))
+    }
+
+
+    @Test
+    fun tryColorThem7() {
+        val doggos = "abcdefghijklmnopqrstuvwxyz".toListOfString()
+        val exp = false
+        assertEquals(exp, tryColorThem(doggos))
+    }
+
+
+}
